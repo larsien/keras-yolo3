888c69a0ff808411c2623cc8bab1844f29091a54
larsien <larsien85@gmail.com>
2020-06-24 18:32:53 +0900

    Feature : Add checkpoint code
    To reuse pre-trained weight in history

--- train.py
+++ train.py
@@ -11,30 +11,49 @@ from keras.callbacks import TensorBoard, ModelCheckpoint, ReduceLROnPlateau, Ear
 
 from yolo3.model import preprocess_true_boxes, yolo_body, tiny_yolo_body, yolo_loss
 from yolo3.utils import get_random_data
+from tensorflow.python.ops import control_flow_ops
+from keras.utils import multi_gpu_model
 
-
+import tensorflow as tf 
+import os 
 def _main():
+    
+#     os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+    os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+    
+    config = tf.ConfigProto()
+    config.gpu_options.allow_growth = True
+    config.allow_soft_placement = True  
+    config.gpu_options.per_process_gpu_memory_fraction = 0.8
+    config.log_device_placement = False 
+    
+    K.set_session(tf.Session(config=config))
+    
+#     print("Num GPUs Available: ", len(tf.config.experimental.list_physical_devices('GPU')))
+
     annotation_path = 'train.txt'
     log_dir = 'logs/000/'
-    classes_path = 'model_data/voc_classes.txt'
-    anchors_path = 'model_data/yolo_anchors.txt'
+    classes_path = 'class.txt'
+    anchors_path = 'model_data/tiny_yolo_anchors.txt'
     class_names = get_classes(classes_path)
     num_classes = len(class_names)
     anchors = get_anchors(anchors_path)
 
     input_shape = (416,416) # multiple of 32, hw
+#     input_shape = (320,320) # multiple of 32, hw
 
     is_tiny_version = len(anchors)==6 # default setting
+    print(f'is tiny ? {is_tiny_version}')
     if is_tiny_version:
         model = create_tiny_model(input_shape, anchors, num_classes,
-            freeze_body=2, weights_path='model_data/tiny_yolo_weights.h5')
+            freeze_body=2, weights_path='model_data/yolo_tiny.h5')
     else:
         model = create_model(input_shape, anchors, num_classes,
             freeze_body=2, weights_path='model_data/yolo_weights.h5') # make sure you know what you freeze
-
+    
     logging = TensorBoard(log_dir=log_dir)
     checkpoint = ModelCheckpoint(log_dir + 'ep{epoch:03d}-loss{loss:.3f}-val_loss{val_loss:.3f}.h5',
-        monitor='val_loss', save_weights_only=True, save_best_only=True, period=3)
+        monitor='val_loss', save_weights_only=True, save_best_only=True, period=1)
     reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=3, verbose=1)
     early_stopping = EarlyStopping(monitor='val_loss', min_delta=0, patience=10, verbose=1)
 
@@ -51,40 +70,101 @@ def _main():
     # Adjust num epochs to your dataset. This step is enough to obtain a not bad model.
     if True:
         model.compile(optimizer=Adam(lr=1e-3), loss={
-            # use custom yolo_loss Lambda layer.
-            'yolo_loss': lambda y_true, y_pred: y_pred})
-
+                # use custom yolo_loss Lambda layer.
+                'yolo_loss': lambda y_true, y_pred: y_pred})
+        
         batch_size = 32
-        print('Train on {} samples, val on {} samples, with batch size {}.'.format(num_train, num_val, batch_size))
-        model.fit_generator(data_generator_wrapper(lines[:num_train], batch_size, input_shape, anchors, num_classes),
+        
+        #add for last checkpoint referenced https://github.com/qqwweee/keras-yolo3/issues/652
+        checkpoint_epoch = 0 
+        if is_history_exist(log_dir):
+            checkpoint_path, checkpoint_epoch = get_minimum_loss_weight(log_dir)
+            model.load_weights(checkpoint_path)
+            if checkpoint_epoch > 50:
+                pass
+            else : 
+                model.fit_generator(data_generator_wrapper(lines[:num_train], batch_size, input_shape, anchors, num_classes),
+                    steps_per_epoch=max(1, num_train//batch_size),
+                    validation_data=data_generator_wrapper(lines[num_train:], batch_size, input_shape, anchors, num_classes),
+                    validation_steps=max(1, num_val//batch_size),
+                    epochs=50,
+                    initial_epoch=checkpoint_epoch,
+                    callbacks=[logging, checkpoint])       
+           
+        else:
+            print('checkpoint files are not exsisting.')
+            model.fit_generator(data_generator_wrapper(lines[:num_train], batch_size, input_shape, anchors, num_classes),
                 steps_per_epoch=max(1, num_train//batch_size),
                 validation_data=data_generator_wrapper(lines[num_train:], batch_size, input_shape, anchors, num_classes),
                 validation_steps=max(1, num_val//batch_size),
                 epochs=50,
                 initial_epoch=0,
                 callbacks=[logging, checkpoint])
-        model.save_weights(log_dir + 'trained_weights_stage_1.h5')
+        
+        if checkpoint_epoch <= 50:
+            print('Train on {} samples, val on {} samples, with batch size {}.'.format(num_train, num_val, batch_size))
+            model.save_weights(log_dir + 'trained_weights_stage_1.h5')
 
+    
     # Unfreeze and continue training, to fine-tune.
     # Train longer if the result is not good.
     if True:
         for i in range(len(model.layers)):
             model.layers[i].trainable = True
-        model.compile(optimizer=Adam(lr=1e-4), loss={'yolo_loss': lambda y_true, y_pred: y_pred}) # recompile to apply the change
+        model.compile(optimizer=Adam(lr=1e-3), loss={'yolo_loss': lambda y_true, y_pred: y_pred}) # recompile to apply the change
         print('Unfreeze all of the layers.')
 
         batch_size = 32 # note that more GPU memory is required after unfreezing the body
         print('Train on {} samples, val on {} samples, with batch size {}.'.format(num_train, num_val, batch_size))
-        model.fit_generator(data_generator_wrapper(lines[:num_train], batch_size, input_shape, anchors, num_classes),
-            steps_per_epoch=max(1, num_train//batch_size),
-            validation_data=data_generator_wrapper(lines[num_train:], batch_size, input_shape, anchors, num_classes),
-            validation_steps=max(1, num_val//batch_size),
-            epochs=100,
-            initial_epoch=50,
-            callbacks=[logging, checkpoint, reduce_lr, early_stopping])
-        model.save_weights(log_dir + 'trained_weights_final.h5')
+        if checkpoint_epoch > 50:
+            model.fit_generator(data_generator_wrapper(lines[:num_train], batch_size, input_shape, anchors, num_classes),
+                steps_per_epoch=max(1, num_train//batch_size),
+                validation_data=data_generator_wrapper(lines[num_train:], batch_size, input_shape, anchors, num_classes),
+                validation_steps=max(1, num_val//batch_size),
+                epochs=100,
+                initial_epoch=checkpoint_epoch,
+                callbacks=[logging, checkpoint, reduce_lr, early_stopping],
+                workers=4, 
+#                 use_multiprocessing=True
+                               )
+#             model.fit()
+        else : 
+            model.fit_generator(data_generator_wrapper(lines[:num_train], batch_size, input_shape, anchors, num_classes),
+                steps_per_epoch=max(1, num_train//batch_size),
+                validation_data=data_generator_wrapper(lines[num_train:], batch_size, input_shape, anchors, num_classes),
+                validation_steps=max(1, num_val//batch_size),
+                epochs=100,
+                initial_epoch=50,
+                callbacks=[logging, checkpoint, reduce_lr, early_stopping])
+#         model.save_weights(log_dir + 'trained_weights_final.h5')
 
     # Further training if needed.
+def is_history_exist(log_dir):
+    history_files = []
+    for file in os.listdir(log_dir):
+        if '.h5' in file:
+            history_files.append(file)
+    
+    if len(history_files) == 0:
+        return False
+    else : 
+        return True
+    
+def get_minimum_loss_weight(log_dir):
+    history_files = []
+    for file in os.listdir(log_dir):
+        if '.h5' in file:
+            history_files.append(file)
+    minimum_loss = float(9999999)
+    minimum_loss_file = ''
+    for file in history_files:
+        loss = float(file.split('-')[1].replace('loss',''))
+        if loss < minimum_loss:
+            minimum_loss = loss
+            minimum_loss_file = file
+    print(f'Minimum loss file : {minimum_loss_file}, minimum loss : {minimum_loss}, ')
+    epoch = int(minimum_loss_file.split('-')[0].replace('ep', ''))
+    return os.path.join(log_dir,minimum_loss_file), epoch
 
 
 def get_classes(classes_path):
@@ -128,6 +208,7 @@ def create_model(input_shape, anchors, num_classes, load_pretrained=True, freeze
     model_loss = Lambda(yolo_loss, output_shape=(1,), name='yolo_loss',
         arguments={'anchors': anchors, 'num_classes': num_classes, 'ignore_thresh': 0.5})(
         [*model_body.output, *y_true])
+    
     model = Model([model_body.input, *y_true], model_loss)
 
     return model
@@ -159,7 +240,7 @@ def create_tiny_model(input_shape, anchors, num_classes, load_pretrained=True, f
         arguments={'anchors': anchors, 'num_classes': num_classes, 'ignore_thresh': 0.7})(
         [*model_body.output, *y_true])
     model = Model([model_body.input, *y_true], model_loss)
-
+    
     return model
 
 def data_generator(annotation_lines, batch_size, input_shape, anchors, num_classes):
